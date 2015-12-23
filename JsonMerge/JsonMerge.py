ed6aa2df4cfb66269700aae02611fb22dea94833
suosuo <1085140725@qq.com>
2015-12-24 00:16:50 +0800

    Add search json file function

--- JsonMerge/JsonMerge.py
+++ JsonMerge/JsonMerge.py
@@ -1,18 +1,55 @@
 import json
 import codecs
-
-import sys,os
+import sys
+import os
 
 path = sys.path[0]
-print path
+#print path  #relative path   os.path.join(dirpath,name)
 
+'''
+#Get the relative path of json file.
 jsonFile = "E:\Shenjing\Python\Python\JsonMerge\CDN\ui\page\page.json"
 jsonData = codecs.open(jsonFile,'r','utf_8_sig')
 jsonString = jsonData.read()
 #print jsonString
 jsonString2 = json.loads(jsonString)
-print jsonString2['Name']
+#print jsonString2['Name']
 
 URL = jsonString2['RelativePath']
-print URL 	
+#print URL 	
 jsonData.close()
+'''
+def getDocOrFile(path):
+	doc = []
+	for i in os.walk(path):
+		doc = doc + i[2]
+	return doc
+	
+def getRelativePath(jsonDoc):
+	jsonData = codecs.open(jsonDoc,'r','utf_8_sig')
+	jsonString = jsonData.read()
+	jsonStringPthon = json.loads(jsonString)
+	return jsonStringPthon['Name']
+	
+docOrFile = getDocOrFile(path)
+print docOrFile
+
+for doc in docOrFile:
+	print type(doc)
+	'''
+	if "json" in doc:
+		Print doc
+		#getRelativePath(doc)
+		'''
+	
+'''
+for docOrFile in os.walk(path):
+	#print docOrFile
+	absolutePath = docOrFile [0]
+	print absolutePath
+	doc = docOrFile[2]  #doc is "yuan zu"
+	for i in doc:
+		#print i
+		if "json" in i:
+			print "ok"  #i is the json doc
+'''
